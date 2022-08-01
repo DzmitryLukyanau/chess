06c64d7dddaf535f617c51347d25b28754012991
Dzmitry Lukyanau <6923643@mail.ru>
2022-08-01 22:10:12 +0300

    first commit

---
+++ src/Bishop.java
@@ -0,0 +1,20 @@
+public class Bishop extends ChessPiece {
+    public Bishop(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+
+    }
+
+    @Override
+    public String getSymbol() {
+        return "B";
+    }
+}
---
+++ src/Pawn.java
@@ -0,0 +1,38 @@
+public class Pawn extends ChessPiece {
+    public Pawn(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (color == "White") {
+            return whiteCanMoveToPosition(chessBoard, line, column, toLine, toColumn);
+        } else return blackCanMoveToPosition(chessBoard, line, column, toLine, toColumn);
+    }
+
+    @Override
+    public String getSymbol() {
+        return "P";
+    }
+
+    public boolean whiteCanMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toColumn == column && toLine > line && chessBoard.checkPos(toLine) && chessBoard.checkPos(toColumn)) {
+            if (line == 1) {
+                return toLine - line <= 2;
+            } else return toLine - line == 1;
+        } else return false;
+    }
+
+    private boolean blackCanMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toColumn == column && toLine < line && chessBoard.checkPos(toLine) && chessBoard.checkPos(toColumn)) {
+            if (line == 6) {
+                return line - toLine <= 2;
+            } else return line - toLine == 1;
+        } else return false;
+    }
+}
